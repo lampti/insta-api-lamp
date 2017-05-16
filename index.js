bb65b53279e3cdbea5ae8e0bde555cea9e60afef
Denner Evaldt <dnrevaldt@gmail.com>
2017-05-16 10:15:36 -0300

    fix

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ app.use(cors());
 
 app.get('/', function (req, res) {
     var hashtag = req.query.hash;
-    
+
     getImagesHash(hashtag, res);
-    
+
 });
 
 function getImagesHash(hashtag, res) {
     var c = new Crawler({
         maxConnections : 10,
-        // This will be called for each crawled page 
+        // This will be called for each crawled page
         callback : function (error, result, $) {
             var item = $("script[type=\"text/javascript\"]").filter(function() {
               return $(this).text().indexOf('window._sharedData') > -1;
             }).text();
             item = item.replace(/(window._sharedData = )/g,"");
             item = item.replace(/(;)/g,"");
-            
+
             var data = JSON.parse(item);
             var nodes = data.entry_data.TagPage[0].tag.media.nodes;
             var urls = [];
@@ -38,7 +38,7 @@ function getImagesHash(hashtag, res) {
         }
     });
 
-    // Queue just one URL, with default callback 
+    // Queue just one URL, with default callback
     c.queue('https://www.instagram.com/explore/tags/'+hashtag+'/');
 }
 
@@ -48,49 +48,44 @@ function getUsers(urls, nodes, res) {
 
     var c = new Crawler({
         maxConnections : 10,
-        // This will be called for each crawled page 
+        // This will be called for each crawled page
         callback : function (error, result, $) {
             var item = $("script[type=\"text/javascript\"]").filter(function() {
               return $(this).text().indexOf('window._sharedData') > -1;
             }).text();
             item = item.replace(/(window._sharedData = )/g,"");
             item = item.replace(/(;)/g,"");
-            
+
             var data = JSON.parse(item);
 
             pages.push(data);
         },
         onDrain : function() {
             var resp = [];
-         
+
             for (var i = 0; i < nodes.length; i++) {
 
                 for (var j = 0; j < pages.length; j++) {
-                    
-                    if (nodes[i].id == pages[j].entry_data.PostPage[0].media.id) {
+                    if (nodes[i].id == pages[j].entry_data.PostPage[0].graphql.shortcode_media.id) {
                         resp.push({
                             img: nodes[i].display_src,
                             caption: nodes[i].caption,
-                            user: pages[j].entry_data.PostPage[0].media.owner.username,
-                            fullname: pages[j].entry_data.PostPage[0].media.owner.full_name
+                            user: pages[j].entry_data.PostPage[0].graphql.shortcode_media.owner.username,
+                            fullname: pages[j].entry_data.PostPage[0].graphql.shortcode_media.owner.full_name
                         });
-                    } 
+                    }
                 }
-                
+
             }
 
             res.json(resp);
         }
     });
 
-    // Queue just one URL, with default callback 
+    // Queue just one URL, with default callback
     c.queue(urls);
 }
 
 var PORT = process.env.PORT || 3000;
 // LISTEN SERVER PORT
 app.listen(PORT);
-
- 
-
- 
